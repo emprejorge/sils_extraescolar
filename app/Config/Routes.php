5c3a37c77ad7099687784efda99a2b8aecb75260
emprejorge <jorge@emprechile.cl>
2026-03-23 16:51:49 -0300

    theme editor

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,7 +18,12 @@ $routes->group('', ['filter' => 'auth'], function ($routes) {
 });
 $routes->group('admin', ['filter' => 'admin'], function ($routes) {
     $routes->get('/', 'Admin\Admin::index');
+
+    // USUARIOS
     $routes->get('usuarios', 'Admin\User::index');
     $routes->get('usuarios/ver/(:num)', 'Admin\User::show/$1');
     $routes->get('usuarios/crear', 'Admin\User::new');
+
+    // CONFIGURACION
+    $routes->get('config/tema', 'Admin\Config\ThemeEditor::index');
 });
